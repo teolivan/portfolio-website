47fb5b968e659a9aa834707eb11972aaece6eaff
olivia <oliviateoliva@gmail.com>
2025-04-11 20:44:26 +0200

    change color theme

--- portfolio-website/src/app/components/Navbar.tsx
+++ portfolio-website/src/app/components/Navbar.tsx
@@ -4,12 +4,12 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="w-full px-6 py-4 bg-blue-950 shadow-sm ">
+        <nav className="w-full px-6 py-4 bg-cyan-900 shadow-sm ">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <Link href="/home" className="text-xl font-semibold text-blue-400 font-montserrat">
-          Home
+        <Link href="/home" className="text-xl font-semibold text-gray-200 font-montserrat">
+          My Portfolio
         </Link>
-        <ul className="flex gap-6 text-sm font-medium text-gray-700 dark:text-gray-300">
+        <ul className="flex gap-6 text-sm font-medium text-gray-200">
           <li>
             <Link href="/">Home</Link>
           </li>
--- portfolio-website/src/app/pages/Homepage.tsx
+++ portfolio-website/src/app/pages/Homepage.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">
-                Welcome to my portfolio!
-            </h1>
-            <p>
-                My name is Olivia, and I am a second year student at Malmoe University, studying systems engingeering.
-            </p>
-
+        <div className='flex justify-center items-center min-h-screen bg-gray-200'>
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4 text-gray-700">
+                    Welcome to my portfolio!
+                </h1>
+                <p className='text-gray-600'>
+                    Hello!👋 My name is Olivia, and I am a second year student at Malmoe University, studying systems engineering.
+                </p>
+            </div>
         </div>
     ); 
 };
