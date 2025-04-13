5ae01823a0d0ed3b2515966a5e617ed42a8f3495
olivia <oliviateoliva@gmail.com>
2025-04-13 13:45:41 +0200

    update resume

--- portfolio-website/src/app/resume/page.tsx
+++ portfolio-website/src/app/resume/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
     <Navbar/>
     <div className="flex justify-center items-center min-h-screen bg-gray-200 p-4">
       <iframe
-        src="resume.pdf"
+        src="olivia-svensson-resume.pdf"
         className="w-full h-[90vh] max-w-4xl border shadow-lg"
         title="Resume"
       />
